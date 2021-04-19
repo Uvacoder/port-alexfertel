3c81d4b40cfa1ccaec307bb6e671823c0ec172fd
Alexander Gonzalez <alexfertel97@gmail.com>
2021-04-19 19:21:59 -0400

    Add dark mode support

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,21 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex min-h-screen">
       <Head>
         <title>Alexander González</title>
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center"></main>
+      <main className="flex items-center justify-between flex-1 px-20">
+        <section className="flex flex-col justify-center">
+          <h1 className="text-9xl text-gray-800 dark:text-gray-100">
+            <span>Hello!</span>
+            <br />
+            <span>I'm Alex.</span>
+          </h1>
+        </section>
+        <section></section>
+      </main>
     </div>
   );
 }
