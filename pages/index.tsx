1def80715bf9882c01afff4624ccc309565ecabb
Alexander Gonzalez <alexfertel97@gmail.com>
2021-04-19 18:55:48 -0400

    Remove vercel stuff and the index page footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,12 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>Alexander González</title>
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center"></main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t"></footer>
     </div>
   );
 }
