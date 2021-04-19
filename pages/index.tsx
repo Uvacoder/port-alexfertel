1e5d5a2ce9150efa7265d69a03435a4afad53c03
Alexander Gonzalez <alexfertel97@gmail.com>
2021-04-19 19:34:30 -0400

    Add a theme switch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
+import { useReducer } from "react";
+import { Switch } from "@headlessui/react";
+import { useTheme } from "next-themes";
 import Head from "next/head";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+  const isDarkTheme = theme === "dark";
+
   return (
     <div className="flex min-h-screen">
       <Head>
@@ -15,7 +21,22 @@ export default function Home() {
             <span>I'm Alex.</span>
           </h1>
         </section>
-        <section></section>
+        <section>
+          <Switch
+            checked={isDarkTheme}
+            onChange={() => setTheme(isDarkTheme ? "light" : "dark")}
+            className={`${
+              isDarkTheme ? "bg-blue-600" : "bg-gray-200"
+            } relative inline-flex items-center h-6 rounded-full w-11`}
+          >
+            <span className="sr-only">Toggle theme</span>
+            <span
+              className={`${
+                isDarkTheme ? "translate-x-6" : "translate-x-1"
+              } inline-block w-4 h-4 transform bg-white rounded-full`}
+            />
+          </Switch>
+        </section>
       </main>
     </div>
   );
