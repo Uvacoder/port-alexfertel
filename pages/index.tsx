ec6972b669ce4db536826606f5589dd3698d56f7
Alexander Gonzalez <alexfertel97@gmail.com>
2021-08-09 23:04:21 -0400

    Remove the animation whatsoever because it is hard on performance

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,23 +14,6 @@ export default function Home() {
         </section>
 
         <section className="relative flex justify-center w-full h-full px-16 text-gray-800 sm:items-center dark:text-coolGray-50">
-          <div className="absolute inset-0 flex items-center justify-around">
-            <svg
-              className="w-80 text-rose-500 dark:text-blueGray-500 blur-2xl"
-              viewBox="0 0 50 50"
-            >
-              <circle className="fill-current" cx="12.5" cy="12.5" r="5">
-                <animateTransform
-                  attributeName="transform"
-                  type="rotate"
-                  from="360 25 25"
-                  to="0 25 25"
-                  dur="20s"
-                  repeatCount="indefinite"
-                />
-              </circle>
-            </svg>
-          </div>
           <h1 className="z-10 text-lg sm:text-xl md:text-2xl lg:text-4xl">
             Hello,{" "}
             <span className="text-rose-500 dark:text-blue-400">I'm Alex</span>.
