ea01e04874b94cb8d148abb5697f43a62df0f468
Alexander Gonzalez <alexfertel97@gmail.com>
2021-08-13 09:29:19 -0400

    Fix open source fetching

--- pages/open-source.tsx
+++ pages/open-source.tsx
@@ -1,54 +1,90 @@
 import * as React from "react";
+import axios from "axios";
 
-export async function getStaticProps() {
-  // {
-  //   "data": {
-  //     "repository": {
-  //       "issue": {
-  //         "reactionGroups": [
-  //           { "content": "THUMBS_UP", "users": { "totalCount": 0 } },
-  //           { "content": "THUMBS_DOWN", "users": { "totalCount": 0 } },
-  //           { "content": "LAUGH", "users": { "totalCount": 0 } },
-  //           { "content": "HOORAY", "users": { "totalCount": 0 } },
-  //           { "content": "CONFUSED", "users": { "totalCount": 0 } },
-  //           { "content": "HEART", "users": { "totalCount": 0 } },
-  //           { "content": "ROCKET", "users": { "totalCount": 0 } },
-  //           { "content": "EYES", "users": { "totalCount": 0 } }
-  //         ]
-  //       }
-  //     }
-  //   }
-  // }
-  const token = process.env.GITHUB_TOKEN;
-  const res = await fetch(`https://api.github.com/user/repos`, {
-    method: "GET",
+const fetchRepoCount = (token) =>
+  axios({
+    method: "get",
+    url: `https://api.github.com/user/repos`,
     headers: {
       Authorization: `bearer ${token}`,
     },
-  });
+  }).then((response) => response.data);
 
-  const repos = await res.json();
-  if (res.status !== 200) {
-    console.error(repos);
-    throw new Error("Failed to fetch API");
-  }
+const fetchCommitCount = (token) =>
+  axios({
+    method: "get",
+    url: `https://api.github.com/search/commits?q=author:alexfertel`,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github.cloak-preview",
+      Authorization: `bearer ${token}`,
+    },
+  }).then((response) => response.data);
+
+const fetchUserInfo = (token) =>
+  axios({
+    method: "post",
+    url: "https://api.github.com/graphql",
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+    data: JSON.stringify({
+      query: `
+      query {
+        user(login: "alexfertel") {
+          name
+          login
+          contributionsCollection {
+            totalCommitContributions
+            restrictedContributionsCount
+          }
+          repositoriesContributedTo(first: 1, contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) {
+            totalCount
+          }
+          pullRequests(first: 1) {
+            totalCount
+          }
+          issues(first: 1) {
+            totalCount
+          }
+          followers {
+            totalCount
+          }
+          repositories(first: 100, ownerAffiliations: OWNER, orderBy: {direction: DESC, field: STARGAZERS}) {
+            totalCount
+            nodes {
+              stargazers {
+                totalCount
+              }
+            }
+          }
+        }
+      }
+      `,
+    }),
+  }).then((response) => response.data);
+
+export async function getStaticProps() {
+  const token = process.env.GITHUB_TOKEN;
+  const responses = await Promise.all([
+    fetchRepoCount(token),
+    fetchCommitCount(token),
+    fetchUserInfo(token),
+  ]);
+
+  console.log(responses[2]);
   return {
-    props: { repos },
+    props: { repos: responses[0], count: responses[1].total_count },
     revalidate: 60,
   };
 }
 
-const OpenSource = ({ repos }) => {
-  const public_repos = repos.filter((r) => !r.private);
-  const private_repos = repos.filter((r) => r.private);
+const OpenSource = ({ repos, count }) => {
   return (
-    <div className="grid place-items-center">
-      {repos.map((repo) => (
-        <pre key={repo}>{JSON.stringify(repo.full_name, null, 2)}</pre>
-      ))}
-      {public_repos.length}
-      {"\n"}
-      {private_repos.length}
+    <div>
+      {repos.length}
+      <br />
+      {count}
     </div>
   );
 };
